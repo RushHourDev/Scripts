c565c6ab4e79c19864a9cce642564f5df6299c9e
RushHourDev <119634792+RushHourDev@users.noreply.github.com>
2025-08-14 00:35:35 -0400

    Create unzip.py

---
+++ unzip.py
@@ -0,0 +1,8 @@
+#Usage: ./unzip.py zippedfile.zip
+
+#!/usr/bin/env python3
+import sys
+from zipfile import PyZipFile
+for zip_file in sys.argv[1:]:
+    pzf = PyZipFile(zip_file)
+    pzf.extractall()
